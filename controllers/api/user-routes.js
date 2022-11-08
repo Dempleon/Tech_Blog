c71b71b1c77e0794403308bd9742a0d5cb266c1c
Dempoleon <dempoleon@gmail.com>
2022-11-07 18:38:07 -0800

    added create route and login route for user

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const {Userj, User} = require('../../models');
+
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            email: req.body.emaill,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            res.status(200).json(userData);
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email,
+            },
+        });
+
+        if(!userData) {
+            res.status(400).json({message: 'Please enter a registered email'});
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({message: 'Please enter the correct password'});
+            return;
+        };
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status.json({user: userData, message: 'You are logged in!'});
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
